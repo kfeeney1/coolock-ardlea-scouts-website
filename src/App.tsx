ef364994216ed4b3a86da2f3254407e4b0687751
kfeeney1 <114672059+kfeeney1@users.noreply.github.com>
2026-08-30 17:13:23 +0100

    Add section cashbook route

--- src/App.tsx
+++ src/App.tsx
@@ -30,6 +30,7 @@ const MeetingRecords = lazy(() => import("./pages/MeetingRecords"));
 const WeeklySectionTracker = lazy(() => import("./pages/WeeklySectionTracker"));
 const BadgeworkTracking = lazy(() => import("./pages/BadgeworkTracking"));
 const EquipmentManagement = lazy(() => import("./pages/EquipmentManagement"));
+const SectionCashbook = lazy(() => import("./pages/SectionCashbook"));
 const OrganisationChart = lazy(() => import("./pages/OrganisationChart"));
 const ConsentManagement = lazy(() => import("./pages/ConsentManagement"));
 const LeaderInfo = lazy(() => import("./pages/LeaderInfo"));
@@ -79,6 +80,7 @@ export default function App() {
               <Route path="/leader/weekly" element={protectedRoute(<WeeklySectionTracker />)} />
               <Route path="/leader/badgework" element={protectedRoute(<BadgeworkTracking />)} />
               <Route path="/leader/equipment" element={protectedRoute(<EquipmentManagement />)} />
+              <Route path="/leader/finance" element={protectedRoute(<SectionCashbook />)} />
               <Route path="/leader/organisation" element={protectedRoute(<OrganisationChart />)} />
               <Route path="/leader/consents" element={protectedRoute(<ConsentManagement />)} />
               <Route path="/leader/info" element={protectedRoute(<LeaderInfo />)} />
