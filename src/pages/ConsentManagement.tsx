b29641765b6cec4603dac02ca3e1724594a613bc
kfeeney1 <114672059+kfeeney1@users.noreply.github.com>
2026-09-23 11:08:34 +0000

    Fix unused imports after page-density cleanup

--- src/pages/ConsentManagement.tsx
+++ src/pages/ConsentManagement.tsx
@@ -1,4 +1,4 @@
-import { Alert, Box, Button, Container } from "@mui/material";
+import { Alert, Box, Container } from "@mui/material";
 import { useEffect, useMemo, useState } from "react";
 
 import ConsentOverviewPanel from "../components/admin/ConsentOverviewPanel";
